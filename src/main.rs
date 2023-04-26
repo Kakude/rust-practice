05ea6e95acdb24f9a5f8cd5e4ffed61614a79ac8
Masahiro Sato <34153654+maakun12@users.noreply.github.com>
2023-04-26 11:54:55 +0900

    add GET: /healthcheck (#21)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+use httparse;
+use diesel::mysql::MysqlConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let url = env::var("URL").expect("URL must be set");
+
+    let listener = TcpListener::bind(&url).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        router(stream);
+        println!("Connection established!");
+    }
 }
+
+fn router(mut stream: TcpStream) {
+    let mut buf = [0; 4096];
+    stream.read(&mut buf).unwrap();
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut req = httparse::Request::new(&mut headers);
+    req.parse(&buf).unwrap();
+    let path = req.path.expect("fail");
+    match path {
+        "/healthcheck" => {
+            establish_connection();
+            let body =
+            "<html><head><title>rust web</title></head><body>hello world!!!!!</body></html>";
+            let status = "HTTP/1.1 200 OK\r\n".to_string();
+            let header = status + "Content-Type: text/html; charset=UTF-8\r\n\r\n";
+            let res = header + &body + "\r\n";
+            let data = res.as_bytes();
+            stream.write(data).unwrap();
+        }
+        &_ => todo!()
+    }
+}
+
+pub fn establish_connection() -> MysqlConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let connection = MysqlConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url));
+
+    println!("mysql connected");
+
+    connection
+ }
